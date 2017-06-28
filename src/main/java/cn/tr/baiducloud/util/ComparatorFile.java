a5de3d90ddac6fe32ed7467ef2161fb7542dfc77
taor <taor@geroncloud.com>
2017-06-28 15:10:10 +0800

    1

--- src/main/java/cn/tr/baiducloud/util/ComparatorFile.java
+++ src/main/java/cn/tr/baiducloud/util/ComparatorFile.java
@@ -6,8 +6,8 @@ import cn.tr.baiducloud.entity.File;
 
 /**
  * 
- * File对象比较类
- * 简介: 根据文件的相似度属性对文件对象比较大小.
+ * 对象比较类
+ * 简介: 根据两个对象的指定属性对两个对象比较大小.
  * 
  * @author taorun
  * @date 2017年6月7日 下午10:25:23
@@ -16,6 +16,9 @@ import cn.tr.baiducloud.entity.File;
 
 public class ComparatorFile implements Comparator<Object> {
 
+	/**
+	 * 根据对象的 score 属性比较大小
+	 */
 	public int compare(Object o1, Object o2) {
 		File file1 = (File) o1;
 		File file2 = (File) o2;
---
+++ src/main/java/cn/tr/baiducloud/util/JavaTest.java
@@ -0,0 +1,28 @@
+package cn.tr.baiducloud.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * java方法测试类
+ * 
+ * @author taorun
+ * @date 2017年6月28日
+ *
+ */
+
+public class JavaTest {
+	
+	public static void main(String[] args) {
+		
+		String string = "1,2,3,4,5";
+		String[] arr = string.split(",");
+		List<String> list = Arrays.asList(arr);
+		for (String s : list) {
+			System.out.println(Integer.parseInt(s) > 3);
+		}
+		System.out.println(list);
+		
+	}
+
+}
